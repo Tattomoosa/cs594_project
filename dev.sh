839f4313e1b7f89f083fd9197d09d2ffe94e9ce3
Matt O'Tousa <mlotousa@gmail.com>
2021-06-02 19:21:20 -0700

    added dev launcher script

---
+++ dev.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+SESSION_NAME=cs594_proj
+PORT=$1
+
+source ./env/bin/activate
+
+if tmux has-session -t $SESSION_NAME &> /dev/null
+then
+    tmux attach -t $SESSION_NAME
+else
+    tmux new -s $SESSION_NAME -d    
+
+    tmux set -g pane-border-status top
+    tmux bind X confirm-before "kill-session -t ''"
+
+    tmux select-pane -T 'server'
+    tmux send-keys -t 0 "./server.py $PORT" C-m
+    tmux split-window -h
+
+    tmux select-pane -T 'client'
+    tmux send-keys -t 1 "./client.py $PORT" C-m
+    tmux split-window -h
+
+    tmux select-pane -T 'client2'
+    tmux send-keys -t 2 "./client.py $PORT" C-m
+
+    tmux a -t $SESSION_NAME
+fi
+    
