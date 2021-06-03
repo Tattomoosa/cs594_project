9a3f02b6f5f3cf772ce9e6c558447e7da7c665d9
Matt O'Tousa <mlotousa@gmail.com>
2021-06-02 19:52:54 -0700

    client hearbeat

--- opcodes.py
+++ opcodes.py
@@ -3,14 +3,15 @@ from dataclasses import dataclass
 @dataclass
 class OpCode:
 
-    ERR = 'ERR'
-    LOGIN = 'LOGIN'
-    LIST_ROOMS = 'LIST_ROOMS'
-    LIST_USERS = 'LIST_USERS'
-    JOIN_ROOM = 'JOIN_ROOM'
-    LEAVE_ROOM = 'LEAVE_ROOM'
-    MESSAGE = 'MESSAGE'
-    USER_EXIT = 'EXIT'
+    ERR = 0x0
+    LOGIN = 0x1
+    LIST_ROOMS = 0x2
+    LIST_USERS = 0x3
+    JOIN_ROOM = 0x4 
+    LEAVE_ROOM = 0x5
+    MESSAGE = 0x6
+    USER_EXIT = 0x7
+
     ERR_UNKNOWN = 0x10
     ERR_ILLEGAL_OP = 0x11
     ERR_ILLEGAL_LEN = 0x12
@@ -18,3 +19,4 @@ class OpCode:
     ERR_ILLEGAL_NAME = 0x14
     ERR_ILLEGAL_MSG = 0x15
     ERR_MALFORMED = 0x16
+    ERR_TIMEOUT = 0x17
